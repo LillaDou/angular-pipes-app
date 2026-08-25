631ce8ae4e485371a1b9954edc163fee0a5c1932
Lilla Doughty <lilla.e.j.du@gmail.com>
2026-08-25 18:14:14 +0200

    i18nPlural Pipe

--- src/app/pages/uncommon-page/uncommon-page.ts
+++ src/app/pages/uncommon-page/uncommon-page.ts
@@ -1,6 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { Card } from '../../components/card/card';
-import { I18nSelectPipe } from '@angular/common';
+import { I18nPluralPipe, I18nSelectPipe } from '@angular/common';
 
 
 const client1 = {
@@ -22,13 +22,15 @@ const client2 = {
   selector: 'app-uncommon-page',
   imports: [
     Card,
-    I18nSelectPipe
+    I18nSelectPipe,
+    I18nPluralPipe,
   ],
   templateUrl: './uncommon-page.html',
 })
 export default class UncommonPage {
 
-  //i18n Select Pipe - internacionalización
+  //i18nSelect Pipe - internacionalización
+  //Aquí lo hemos usado para cambiar la palabra 'invitarlo/a' en base al género al presionar el botón
   client = signal(client1);
 
   invitationMap = {
@@ -43,6 +45,30 @@ export default class UncommonPage {
     }
 
     this.client.set(client1);
+  }
+
+  //i18n Plural Pipe - funciona cuando tenemos varios datos/colecciones de valores/diferentes elementos
+
+  clientsMap = signal({
+    '=0': 'no tenemos ningún cliente esperando',
+    '=1': 'tenemos un cliente esperando',
+    '=2': 'tenemos 2 clientes esperando',
+    other: 'tenemos # clientes esperando'
+  })
+
+  clients = signal([
+    'Maria',
+    'Pedro',
+    'Fernando',
+    'Melissa',
+    'Natalia',
+    'Andrea',
+    'Juan',
+    'Carlos',
+  ]);
+
+  deleteClient(){
+    this.clients.update( prev => prev.slice(1) );
   }
 
 
