74522d0a0bcc15076f353ea5ef23862f05309f49
Lilla Doughty <lilla.e.j.du@gmail.com>
2026-08-25 17:51:20 +0200

    i18n Select Pipe

--- src/app/pages/uncommon-page/uncommon-page.ts
+++ src/app/pages/uncommon-page/uncommon-page.ts
@@ -1,11 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { Card } from '../../components/card/card';
+import { I18nSelectPipe } from '@angular/common';
+
+
+const client1 = {
+  name: 'Stefan',
+  gender: 'male',
+  age: 32,
+  address: 'San Fernando, Spain'
+};
+
+const client2 = {
+  name: 'Lilla',
+  gender: 'female',
+  age: 28,
+  address: 'Firgas, Spain'
+}
+
 
 @Component({
   selector: 'app-uncommon-page',
   imports: [
-    Card
+    Card,
+    I18nSelectPipe
   ],
   templateUrl: './uncommon-page.html',
 })
-export default class UncommonPage {}
+export default class UncommonPage {
+
+  //i18n Select Pipe
+  client = signal(client1);
+
+  invitationMap = {
+    male: 'invitarlo',
+    female: 'invitarla',
+  }
+
+  changeClient() {
+    if( this.client() === client1 ) {
+      this.client.set(client2);
+      return;
+    }
+
+    this.client.set(client1);
+  }
+
+
+}
