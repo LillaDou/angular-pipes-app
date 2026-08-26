b0fa2fb12682fca7bf46e5bca43393b128ffc1b5
Lilla Doughty <lilla.e.j.du@gmail.com>
2026-08-26 15:22:31 +0200

    JsonPipe

--- src/app/pages/uncommon-page/uncommon-page.ts
+++ src/app/pages/uncommon-page/uncommon-page.ts
@@ -1,6 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { Card } from '../../components/card/card';
-import { I18nPluralPipe, I18nSelectPipe, SlicePipe } from '@angular/common';
+import { I18nPluralPipe, I18nSelectPipe, JsonPipe, SlicePipe, UpperCasePipe } from '@angular/common';
 
 
 const client1 = {
@@ -25,6 +25,8 @@ const client2 = {
     I18nSelectPipe,
     I18nPluralPipe,
     SlicePipe,
+    JsonPipe,
+    UpperCasePipe
   ],
   templateUrl: './uncommon-page.html',
 })
